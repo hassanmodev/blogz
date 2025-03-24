d542d9d83fb56d8e2dab60e9bb63bf371dacb83b
Hassan Mohamed <se7s.mohamed26@gmail.com>
2025-03-24 13:23:58 +0200

    Fixed test import

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 
 @Injectable()
